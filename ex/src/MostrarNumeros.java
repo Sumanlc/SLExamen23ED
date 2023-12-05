082f79ad0cb1d0e4e108b060be94f5759a49c4f1
Suman_Lamichhane <lamichhanesuman79@gmail.com>
2023-12-05 13:32:32 +0100

    Segundo.1 cambio en Rama RESTADORES

--- ex/src/MostrarNumeros.java
+++ ex/src/MostrarNumeros.java
@@ -57,6 +57,26 @@ public class MostrarNumeros {
 
     }
 
+    public static double restar(double a, double b) {
+
+        return a - b;
+
+    }
+
+    public static double dividir(double a, double b) {
+
+        if (b != 0) {
+
+            return a / b;
+
+        } else {
+
+            throw new IllegalArgumentException("No se puede dividir por cero");
+
+        }
+
+    }
+
     public static double raizCuadrada(double a) {
 
         if (a >= 0) {
@@ -68,8 +88,6 @@ public class MostrarNumeros {
             throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
 
         }
-
-
 
     }
 
